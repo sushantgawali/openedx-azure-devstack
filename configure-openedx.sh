929497df98108ccdebd6b00a26917d31cb14895c
sushantgawali <sushant.gawali@gmail.com>
2016-05-02 12:40:38 +0530

    Removed edx install script

--- configure-openedx.sh
+++ configure-openedx.sh
@@ -69,39 +69,5 @@ ensureAzureNetwork()
 }
 ensureAzureNetwork
 
-###################################################
-# Update Ubuntu and install prereqs
-###################################################
-
-time sudo apt-get -y update && sudo apt-get -y upgrade
-time sudo apt-get install -y build-essential software-properties-common python-software-properties curl git-core libxml2-dev libxslt1-dev libfreetype6-dev python-pip python-apt python-dev libxmlsec1-dev swig
-time sudo pip install --upgrade pip
-time sudo pip install --upgrade virtualenv
-
-###################################################
-# Pin specific version of Open edX (named-release/cypress for now)
-###################################################
-export OPENEDX_RELEASE='named-release/dogwood.rc'
-EXTRA_VARS="-e edx_platform_version=$OPENEDX_RELEASE \
-  -e certs_version=$OPENEDX_RELEASE \
-  -e forum_version=$OPENEDX_RELEASE \
-  -e xqueue_version=$OPENEDX_RELEASE \
-  -e configuration_version=appsembler/azureDeploy \
-  -e edx_ansible_source_repo=https://github.com/appsembler/configuration \
-"
-
-###################################################
-# Download configuration repo and start ansible
-###################################################
-
-cd /tmp
-time git clone https://github.com/appsembler/configuration.git
-cd configuration
-time git checkout appsembler/azureDeploy
-time sudo pip install -r requirements.txt
-cd playbooks
-
-sudo ansible-playbook -i localhost, -c local vagrant-devstack.yml $EXTRA_VARS
-
 date
 echo "completed open edx devstack provision on pid $$"
